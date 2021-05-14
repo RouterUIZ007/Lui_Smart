f8f3424972435e3ab701201dbf0800fce529a780
RouterUIZ007 <router.uiz.360@gmail.com>
2021-05-13 23:29:07 -0500

    base de datos

---
+++ bd/luismart.sql
@@ -0,0 +1,319 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-05-2021 a las 06:28:14
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 8.0.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `luismart`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_c` int(11) NOT NULL,
+  `nombre` varchar(60) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `telefono` varchar(15) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `calle` varchar(255) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `numero` int(11) DEFAULT NULL,
+  `colonia` varchar(255) COLLATE utf8mb4_spanish_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`id_c`, `nombre`, `telefono`, `calle`, `numero`, `colonia`) VALUES
+(1, 'Dani', '123123123123', 'calle', NULL, 'col');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `id_e` int(11) NOT NULL,
+  `nombre` varchar(60) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `fecha_ingreso` date DEFAULT NULL,
+  `fecha_nacimiento` date DEFAULT NULL,
+  `turno` varchar(15) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `id_te` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `presupuesto`
+--
+
+CREATE TABLE `presupuesto` (
+  `folio_p` int(11) NOT NULL,
+  `fecha` date DEFAULT NULL,
+  `id_v` int(11) DEFAULT NULL,
+  `costo` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `presupuesto_servicio`
+--
+
+CREATE TABLE `presupuesto_servicio` (
+  `folio_p` int(11) DEFAULT NULL,
+  `codigo` int(11) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `servicio`
+--
+
+CREATE TABLE `servicio` (
+  `codigo` int(11) NOT NULL,
+  `tipo` varchar(80) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `descripcion` text COLLATE utf8mb4_spanish_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_empleado`
+--
+
+CREATE TABLE `tipo_empleado` (
+  `id_te` int(11) NOT NULL,
+  `tipo` varchar(255) COLLATE utf8mb4_spanish_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` text COLLATE utf8mb4_spanish_ci NOT NULL,
+  `usuario` text COLLATE utf8mb4_spanish_ci NOT NULL,
+  `password` text COLLATE utf8mb4_spanish_ci NOT NULL,
+  `rol` text COLLATE utf8mb4_spanish_ci NOT NULL,
+  `foto` text COLLATE utf8mb4_spanish_ci NOT NULL,
+  `estado` int(11) NOT NULL,
+  `ultimo_login` datetime NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `usuario`, `password`, `rol`, `foto`, `estado`, `ultimo_login`, `fecha`) VALUES
+(7, 'Alexis', 'alexis', '$2a$07$asxx54ahjppf45sd87a5auwbT3U74NyPrrhU4hwJRTdJ/20zuC.bK', 'Cajero', '', 0, '0000-00-00 00:00:00', '2021-05-11 02:30:58'),
+(8, 'Raúl Acevedo Flores', 'raul', '$2a$07$asxx54ahjppf45sd87a5auv7UPUslMtVdBRUG/fW42//pNjvPm9ZC', 'Gerente', '', 0, '0000-00-00 00:00:00', '2021-05-11 19:14:50'),
+(9, 'Abimael', 'root', '$2a$07$asxx54ahjppf45sd87a5auNOqyQU6vZUWwwmFM.tBJOLW4X/5sf0y', 'administrador', '', 0, '0000-00-00 00:00:00', '2021-05-14 04:11:42');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vehiculo`
+--
+
+CREATE TABLE `vehiculo` (
+  `id_v` int(11) NOT NULL,
+  `Matricula` varchar(50) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `marca` varchar(50) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `color` varchar(55) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `obeservaciones` text COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `id_c` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE `venta` (
+  `folio_v` int(11) NOT NULL,
+  `fecha` date DEFAULT NULL,
+  `folio_p` int(11) DEFAULT NULL,
+  `id_e` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_c`);
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`id_e`),
+  ADD KEY `id_te` (`id_te`);
+
+--
+-- Indices de la tabla `presupuesto`
+--
+ALTER TABLE `presupuesto`
+  ADD PRIMARY KEY (`folio_p`),
+  ADD KEY `id_v` (`id_v`);
+
+--
+-- Indices de la tabla `presupuesto_servicio`
+--
+ALTER TABLE `presupuesto_servicio`
+  ADD KEY `codigo` (`codigo`),
+  ADD KEY `folio_p` (`folio_p`);
+
+--
+-- Indices de la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  ADD PRIMARY KEY (`codigo`);
+
+--
+-- Indices de la tabla `tipo_empleado`
+--
+ALTER TABLE `tipo_empleado`
+  ADD PRIMARY KEY (`id_te`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `vehiculo`
+--
+ALTER TABLE `vehiculo`
+  ADD PRIMARY KEY (`id_v`),
+  ADD KEY `id_c` (`id_c`);
+
+--
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`folio_v`),
+  ADD KEY `folio_p` (`folio_p`),
+  ADD KEY `id_e` (`id_e`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_c` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  MODIFY `id_e` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `presupuesto`
+--
+ALTER TABLE `presupuesto`
+  MODIFY `folio_p` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_empleado`
+--
+ALTER TABLE `tipo_empleado`
+  MODIFY `id_te` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `vehiculo`
+--
+ALTER TABLE `vehiculo`
+  MODIFY `id_v` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `folio_v` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD CONSTRAINT `empleado_ibfk_1` FOREIGN KEY (`id_te`) REFERENCES `tipo_empleado` (`id_te`);
+
+--
+-- Filtros para la tabla `presupuesto`
+--
+ALTER TABLE `presupuesto`
+  ADD CONSTRAINT `presupuesto_ibfk_1` FOREIGN KEY (`id_v`) REFERENCES `vehiculo` (`id_v`);
+
+--
+-- Filtros para la tabla `presupuesto_servicio`
+--
+ALTER TABLE `presupuesto_servicio`
+  ADD CONSTRAINT `presupuesto_servicio_ibfk_1` FOREIGN KEY (`codigo`) REFERENCES `servicio` (`codigo`),
+  ADD CONSTRAINT `presupuesto_servicio_ibfk_2` FOREIGN KEY (`folio_p`) REFERENCES `presupuesto` (`folio_p`);
+
+--
+-- Filtros para la tabla `vehiculo`
+--
+ALTER TABLE `vehiculo`
+  ADD CONSTRAINT `vehiculo_ibfk_1` FOREIGN KEY (`id_c`) REFERENCES `cliente` (`id_c`);
+
+--
+-- Filtros para la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD CONSTRAINT `venta_ibfk_1` FOREIGN KEY (`folio_p`) REFERENCES `presupuesto` (`folio_p`),
+  ADD CONSTRAINT `venta_ibfk_2` FOREIGN KEY (`id_e`) REFERENCES `empleado` (`id_e`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

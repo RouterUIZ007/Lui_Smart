ef1bf16998450a11e7b098e84ae677b43843b7b1
RouterUIZ007 <router.uiz.360@gmail.com>
2021-05-14 02:25:07 -0500

    "servicio xD"

--- vistas/modulos/presupuesto.php
+++ vistas/modulos/presupuesto.php
@@ -46,7 +46,7 @@
     <div class="box">
       <!-- Header box -->
       <div class="box-header with-border">
-        <h3 class="box-title">Agregar Presupuesto</h3>
+        <h3 class="box-title">Agregar Servicio</h3>
         <div class="box-tools pull-right">
           <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
             <i class="fa fa-minus"></i></button>

b791f1e89a4fa128390b1c728500046ef2601cb2
RouterUIZ007 <router.uiz.360@gmail.com>
2021-06-11 04:22:53 -0500

    "bd x2 jajajjaa"

--- bd/luismart.sql
+++ bd/luismart.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-06-2021 a las 11:18:40
+-- Tiempo de generación: 11-06-2021 a las 11:22:26
 -- Versión del servidor: 10.4.18-MariaDB
 -- Versión de PHP: 8.0.5
 
@@ -20,6 +20,44 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `luismart`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_c` int(11) NOT NULL,
+  `nombre` varchar(60) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `telefono` varchar(15) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `calle` varchar(255) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `inter` varchar(5) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `colonia` varchar(255) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `exter` varchar(5) COLLATE utf8mb4_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`id_c`, `nombre`, `telefono`, `calle`, `inter`, `colonia`, `exter`) VALUES
+(26, 'LUIS', '9516549873', 'HOLAS', '98', 'BUENA VIUSTA SSSSSS', 'A7');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `id_e` int(11) NOT NULL,
+  `nombre` varchar(60) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `fecha_ingreso` date DEFAULT NULL,
+  `fecha_nacimiento` date DEFAULT NULL,
+  `turno` varchar(15) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `id_te` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 -- --------------------------------------------------------
 
@@ -43,9 +81,132 @@ INSERT INTO `presupuesto` (`folio_p`, `fecha`, `id_v`, `total`) VALUES
 (15, '2021-06-02', 12, '1200.00'),
 (17, '2021-06-04', 14, '56.00');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `servicio`
+--
+
+CREATE TABLE `servicio` (
+  `codigo` int(11) NOT NULL,
+  `concepto` text COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `costo` decimal(10,2) DEFAULT NULL,
+  `tipo` varchar(80) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `Id_v` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `servicio`
+--
+
+INSERT INTO `servicio` (`codigo`, `concepto`, `costo`, `tipo`, `Id_v`) VALUES
+(1, NULL, NULL, NULL, NULL),
+(37, 'PINTURA NEGRA', '800.00', 'PINTURA', 12),
+(38, 'COBRO DE PINTADA', '400.00', 'PINTURA', 12),
+(40, 'ASD', '456.00', 'PINTURA', 12),
+(45, '1', '1.00', 'HOJALATERIA', 14),
+(46, '2', '2.00', 'HOJALATERIA', 14),
+(47, '3', '3.00', 'HOJALATERIA', 14),
+(48, '4', '4.00', 'PINTURA', 14),
+(49, '5', '5.00', 'HOJALATERIA', 14),
+(50, '6', '6.00', 'HOJALATERIA', 14),
+(51, '7', '7.00', 'HOJALATERIA', 14),
+(52, '8', '8.00', 'HOJALATERIA', 14),
+(53, '9', '9.00', 'HOJALATERIA', 14),
+(54, '0', '0.00', 'HOJALATERIA', 14),
+(55, '11', '11.00', 'PINTURA', 14);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_empleado`
+--
+
+CREATE TABLE `tipo_empleado` (
+  `id_te` int(11) NOT NULL,
+  `tipo` varchar(255) COLLATE utf8mb4_spanish_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` text COLLATE utf8mb4_spanish_ci NOT NULL,
+  `usuario` text COLLATE utf8mb4_spanish_ci NOT NULL,
+  `password` text COLLATE utf8mb4_spanish_ci NOT NULL,
+  `rol` text COLLATE utf8mb4_spanish_ci NOT NULL,
+  `foto` text COLLATE utf8mb4_spanish_ci NOT NULL,
+  `estado` int(11) NOT NULL,
+  `ultimo_login` datetime NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `usuario`, `password`, `rol`, `foto`, `estado`, `ultimo_login`, `fecha`) VALUES
+(9, 'admin', 'root', '$2a$07$asxx54ahjppf45sd87a5auNOqyQU6vZUWwwmFM.tBJOLW4X/5sf0y', 'administrador', '', 1, '2021-06-11 03:38:02', '2021-06-11 08:38:02'),
+(12, 'xxx', 'xxx', '$2a$07$asxx54ahjppf45sd87a5auxk2cXQ.31g3rBG7bmaL.q.jFcrQh9Q2', 'administrador', '', 1, '2021-05-30 22:51:25', '2021-05-31 03:51:25');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vehiculo`
+--
+
+CREATE TABLE `vehiculo` (
+  `id_v` int(11) NOT NULL,
+  `Matricula` varchar(50) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `marca` varchar(50) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `color` varchar(55) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `observaciones` text COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `id_c` int(11) DEFAULT NULL,
+  `modelo` varchar(40) COLLATE utf8mb4_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `vehiculo`
+--
+
+INSERT INTO `vehiculo` (`id_v`, `Matricula`, `marca`, `color`, `observaciones`, `id_c`, `modelo`) VALUES
+(1, NULL, NULL, NULL, NULL, NULL, ''),
+(12, 'XX0022', 'FORD', 'NEGRO', 'HALOS SOY UNA CAMIONETASASDASDASDASD', 26, 'F150'),
+(14, 'XX0000', 'MARCA', 'COL', 'ASDASD', 26, 'MOLDE');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE `venta` (
+  `folio_v` int(11) NOT NULL,
+  `fecha` date DEFAULT NULL,
+  `folio_p` int(11) DEFAULT NULL,
+  `id_e` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_c`);
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`id_e`),
+  ADD KEY `id_te` (`id_te`);
 
 --
 -- Indices de la tabla `presupuesto`
@@ -55,8 +216,54 @@ ALTER TABLE `presupuesto`
   ADD KEY `id_v` (`id_v`);
 
 --
+-- Indices de la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  ADD PRIMARY KEY (`codigo`),
+  ADD KEY `Id_v` (`Id_v`);
+
+--
+-- Indices de la tabla `tipo_empleado`
+--
+ALTER TABLE `tipo_empleado`
+  ADD PRIMARY KEY (`id_te`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `vehiculo`
+--
+ALTER TABLE `vehiculo`
+  ADD PRIMARY KEY (`id_v`),
+  ADD KEY `id_c` (`id_c`);
+
+--
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`folio_v`),
+  ADD KEY `folio_p` (`folio_p`),
+  ADD KEY `id_e` (`id_e`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_c` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  MODIFY `id_e` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `presupuesto`
@@ -65,14 +272,69 @@ ALTER TABLE `presupuesto`
   MODIFY `folio_p` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
+-- AUTO_INCREMENT de la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_empleado`
+--
+ALTER TABLE `tipo_empleado`
+  MODIFY `id_te` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de la tabla `vehiculo`
+--
+ALTER TABLE `vehiculo`
+  MODIFY `id_v` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `folio_v` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD CONSTRAINT `empleado_ibfk_1` FOREIGN KEY (`id_te`) REFERENCES `tipo_empleado` (`id_te`);
 
 --
 -- Filtros para la tabla `presupuesto`
 --
 ALTER TABLE `presupuesto`
   ADD CONSTRAINT `presupuesto_ibfk_1` FOREIGN KEY (`id_v`) REFERENCES `vehiculo` (`id_v`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  ADD CONSTRAINT `servicio_ibfk_1` FOREIGN KEY (`Id_v`) REFERENCES `vehiculo` (`id_v`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `vehiculo`
+--
+ALTER TABLE `vehiculo`
+  ADD CONSTRAINT `vehiculo_ibfk_1` FOREIGN KEY (`id_c`) REFERENCES `cliente` (`id_c`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD CONSTRAINT `venta_ibfk_1` FOREIGN KEY (`folio_p`) REFERENCES `presupuesto` (`folio_p`),
+  ADD CONSTRAINT `venta_ibfk_2` FOREIGN KEY (`id_e`) REFERENCES `empleado` (`id_e`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

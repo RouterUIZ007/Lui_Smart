6010e68206b7c035aff6ea9a6f803b1d176a2818
RouterUIZ007 <router.uiz.360@gmail.com>
2021-06-21 07:27:09 -0500

    'BD pdfs'

--- bd/luismart.sql
+++ bd/luismart.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-06-2021 a las 10:18:11
+-- Tiempo de generación: 21-06-2021 a las 14:26:48
 -- Versión del servidor: 10.4.18-MariaDB
 -- Versión de PHP: 8.0.5
 
@@ -42,7 +42,8 @@ CREATE TABLE `cliente` (
 --
 
 INSERT INTO `cliente` (`id_c`, `nombre`, `telefono`, `calle`, `inter`, `colonia`, `exter`) VALUES
-(26, 'LUIS', '9516549873', 'H', '98', 'BUENA VIUSTA SSSSSS', 'A7');
+(26, 'LUIS', '9516549873', 'H', '98', 'BUENA VIUSTA SSSSSS', 'A7'),
+(27, 'ISAI', '9518748745', 'DOMICILIO CONOCIDO', 'S/N', 'MORELOS', 'S/N');
 
 -- --------------------------------------------------------
 
@@ -64,7 +65,26 @@ CREATE TABLE `presupuesto` (
 INSERT INTO `presupuesto` (`folio_p`, `fecha`, `id_v`, `total`) VALUES
 (15, '2021-06-02', 12, '1200.00'),
 (17, '2021-06-04', 14, '56.00'),
-(18, '2021-06-15', 14, '5056.00');
+(18, '2021-06-15', 14, '5056.00'),
+(19, '2021-06-21', 14, '5052.00'),
+(20, '2021-06-21', 15, '2900.00'),
+(21, '2021-06-21', 15, '2900.00'),
+(22, '2021-06-21', 15, '2900.00'),
+(23, '2021-06-21', 15, '2900.00'),
+(24, '2021-06-21', 15, '2900.00'),
+(25, '2021-06-21', 15, '2900.00'),
+(26, '2021-06-21', 15, '2900.00'),
+(27, '2021-06-21', 15, '2900.00'),
+(28, '2021-06-21', 15, '2900.00'),
+(29, '2021-06-21', 15, '2900.00'),
+(30, '2021-06-21', 15, '2900.00'),
+(31, '2021-06-21', 15, '2900.00'),
+(32, '2021-06-21', 15, '2900.00'),
+(33, '2021-06-21', 15, '2900.00'),
+(34, '2021-06-21', 15, '2900.00'),
+(35, '2021-06-21', 15, '2900.00'),
+(36, '2021-06-21', 15, '2900.00'),
+(37, '2021-06-21', 15, '2900.00');
 
 -- --------------------------------------------------------
 
@@ -97,7 +117,11 @@ INSERT INTO `servicio` (`codigo`, `concepto`, `costo`, `tipo`, `Id_v`) VALUES
 (52, '8', '8.00', 'HOJALATERIA', 14),
 (53, '9', '9.00', 'HOJALATERIA', 14),
 (55, '11', '11.00', 'PINTURA', 14),
-(56, '242342', '5000.00', 'PINTURA', 14);
+(56, '242342', '5000.00', 'PINTURA', 14),
+(57, 'PINTURA', '500.00', 'PINTURA', 15),
+(58, 'PASTA', '900.00', 'HOJALATERIA', 15),
+(59, 'PINTADA', '500.00', 'PINTURA', 15),
+(60, 'MANO DE HOBRA', '1000.00', 'HOJALATERIA', 15);
 
 -- --------------------------------------------------------
 
@@ -122,7 +146,7 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombre`, `usuario`, `password`, `rol`, `foto`, `estado`, `ultimo_login`, `fecha`) VALUES
-(9, 'admin', 'root', '$2a$07$asxx54ahjppf45sd87a5auNOqyQU6vZUWwwmFM.tBJOLW4X/5sf0y', 'administrador', '', 1, '2021-06-20 00:52:16', '2021-06-20 05:52:16'),
+(9, 'admin', 'root', '$2a$07$asxx54ahjppf45sd87a5auNOqyQU6vZUWwwmFM.tBJOLW4X/5sf0y', 'administrador', '', 1, '2021-06-21 05:05:19', '2021-06-21 10:05:19'),
 (12, 'xxx', 'xxx', '$2a$07$asxx54ahjppf45sd87a5auxk2cXQ.31g3rBG7bmaL.q.jFcrQh9Q2', 'administrador', '', 1, '2021-05-30 22:51:25', '2021-05-31 03:51:25'),
 (13, 'Jose', 'admin', '$2a$07$asxx54ahjppf45sd87a5auXBm1Vr2M1NV5t/zNQtGHGpS5fFirrbG', 'administrador', '', 1, '0000-00-00 00:00:00', '2021-06-16 01:53:19');
 
@@ -148,7 +172,8 @@ CREATE TABLE `vehiculo` (
 
 INSERT INTO `vehiculo` (`id_v`, `Matricula`, `marca`, `color`, `observaciones`, `id_c`, `modelo`) VALUES
 (12, 'XX0022', 'FORD', 'NEGRO', 'HALOS SOY ASDASDASD', 26, 'F150'),
-(14, 'XX0000', 'MARCA', 'COL3456789', 'ASDASD', 26, 'MOLDE');
+(14, 'XX0000', 'MARCA', 'COL3456789', 'ASDASD', 26, 'MOLDE'),
+(15, 'GW4591', 'FORD', 'ROJO', 'EL CARRO YA TENIA GOLPES EN LA PUERTA DERECHA', 27, 'MUSTANG');
 
 -- --------------------------------------------------------
 
@@ -172,7 +197,16 @@ CREATE TABLE `venta` (
 --
 
 INSERT INTO `venta` (`folio_v`, `fecha`, `folio_p`, `id_e`, `total`, `subtotal`, `cantidad`, `cambio`) VALUES
-(64, '2021-06-20', 17, 9, 64.96, 56, 70, 5.04);
+(64, '2021-06-20', 17, 9, 64.96, 56, 70, 5.04),
+(65, '2021-06-21', 18, 9, 5864.96, 5056, 6000, 135.04),
+(66, '2021-06-21', 18, 9, 5864.96, 5056, 10000, 4135.04),
+(67, '2021-06-21', 17, 9, 64.96, 56, 500, 435.04),
+(68, '2021-06-21', 15, 9, 1392, 1200, 5000, 3608),
+(69, '2021-06-21', 18, 9, 5864.96, 5056, 8000, 2135.04),
+(70, '2021-06-21', 17, 9, 64.96, 56, 1000, 935.04),
+(71, '2021-06-21', 17, 9, 64.96, 56, 5000, 4935.04),
+(72, '2021-06-21', 17, 9, 64.96, 56, 560, 495.04),
+(73, '2021-06-21', 15, 9, 1392, 1200, 3000, 1608);
 
 --
 -- Índices para tablas volcadas
@@ -227,19 +261,19 @@ ALTER TABLE `venta`
 -- AUTO_INCREMENT de la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id_c` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id_c` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT de la tabla `presupuesto`
 --
 ALTER TABLE `presupuesto`
-  MODIFY `folio_p` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `folio_p` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT de la tabla `servicio`
 --
 ALTER TABLE `servicio`
-  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
@@ -251,13 +285,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `vehiculo`
 --
 ALTER TABLE `vehiculo`
-  MODIFY `id_v` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_v` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `venta`
 --
 ALTER TABLE `venta`
-  MODIFY `folio_v` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+  MODIFY `folio_v` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
 
 --
 -- Restricciones para tablas volcadas
